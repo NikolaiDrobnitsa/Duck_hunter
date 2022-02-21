4bfed79ba38d1538bada8b64d1e18cc2391a5ee0
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-02-21 18:01:24 +0200

    add work branch

--- Duck_hunter/Form1.Designer.cs
+++ Duck_hunter/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace Duck_hunter
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Duck_hunter";
         }
 
         #endregion
